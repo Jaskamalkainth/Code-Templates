a0dca554d7c7a97499596008f03f9f51958a4baa
Jaskamal Kainth <kainthjaskamal@gmail.com>
2015-12-20 18:49:29 +0530

    Longest common subsequence

---
+++ longest_common_subsequence.cpp
@@ -0,0 +1,55 @@
+/*
+ *
+ *
+ * O(n*m) complexity , can be reduced to O(n*m/wt) where wt is alphabet size ( set of char's we can use )
+ *
+ */
+
+
+string LCS ( string s , string t )
+{
+	int len_s =  s.length();
+	int len_t = t.length();
+	int dp[s.length()+1][t.length()+1];
+	for(int i = 0; i <= len_s; i++)
+	{
+		for(int j = 0; j <= len_t; j++)
+		{
+			if ( i == 0 || j == 0 ) dp[i][j] =  0;
+			else if ( s[i-1] == t[j-1] )
+				dp[i][j] = dp[i-1][j-1] + 1;
+			else
+				dp[i][j] = max ( dp[i-1][j] , dp[i][j-1] );
+		}
+	}
+	int len_of_lcs = dp[len_s][len_t];
+	/*
+	 * Will return LCS length 
+	 */
+	//return dp[len_s][len_t];   //  Use if only length of LCS is required
+
+	string ans;
+	ans.resize(len_of_lcs);
+
+	int i = len_s;
+	int j = len_t;
+	int id = len_of_lcs;
+
+	while(i > 0 && j > 0)
+	{
+		if (s[i-1] == t[j-1] )
+		{
+			ans[id-1] = s[i-1]; 
+			id--;
+			i--; j--;
+		}
+		else if ( dp[i-1][j] > dp[i][j-1] )
+			i--;  
+		else j--;
+	}
+	/*
+	 * will return string ( longest commone subsequence ) 
+	 */
+	return ans;
+}
+
