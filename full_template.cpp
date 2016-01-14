16721024b41f4b9af6ba2c8d53a48df1a1c7aad1
Jaskamal Kainth <kainthjaskamal@gmail.com>
2016-01-14 18:08:41 +0530

    templates added

--- full_template.cpp
+++ full_template.cpp
@@ -15,6 +15,7 @@
 #include <cstring>
 #include <list>
 #include <map>
+#include <unordered_map>
 #include <iomanip>
 #include <algorithm>
 #include <functional>
@@ -39,14 +40,23 @@ typedef vector<long long>  vll;
 #define mp                 make_pair
 #define pb                 push_back
 #define all(a)             a.begin(),a.end()
-#define pr(n)              printf("%d",n)
-#define s(n)               scanf("%d",&n)
 #define debug(x)	       {cerr <<#x<<" = " <<x<<"\n"; }
 #define debug2(x, y)       {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<"\n";}
 #define ss                 second
 #define ff                 first
 #define m0(x) 		       memset(x,0,sizeof(x))
 #define snuke(c,itr)       for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
+
+template<class T>
+inline bool ispow2(T x){return (x!=0 && (x&(x-1))==0);} //0 or 1
+
+template<class T> inline T powmod(T a,T b,T mod) {ll res = 1; while(b){if(b&1) res = (res*a)%mod;a = (a*a)%mod;b >>= 1;}return res;}
+template<class T> inline T gcd(T a,T b){ll t;while(b){a=a%b;t=a;a=b;b=t;}return a;}
+template<class T> inline T lcm(T a,T b){return a/gcd(a,b)*b;}
+
+inline int nextint(){ int x; scanf("%d",&x); return x; }
+inline ll nextll(){ ll x; scanf("%lld",&x); return x; }
+
 const int mod=1e9+7;
 
 const ll  mx_ll   = numeric_limits<ll> :: max();
@@ -54,32 +64,11 @@ const int mx_int  = numeric_limits<int> :: max();
 
 const long double PI = (long double)(3.1415926535897932384626433832795);
 
-inline bool ispow2(int x){return (x!=0 && (x&(x-1))==0);} //0 or 1
-
-int msb(unsigned x){ union { double a; int b[2]; }; a = x; return (b[1] >> 20) - 1023; }
-
-template<class T>
-inline void cinarr(T a, int n){ for (int i=0;i<n;++i) cin >> a[i];}
-
-inline ll powmod(ll a,ll b) {ll res = 1; while(b){if(b&1) res = (res*a)%mod;a = (a*a)%mod;b >>= 1;}return res;}
-
-inline ll gcd(ll a,ll b){ll t;while(b){a=a%b;t=a;a=b;b=t;}return a;}
-inline ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
-
-inline int nextint(){ int x; scanf("%d",&x); return x; }
-inline ll nextll(){ ll x; scanf("%lld",&x); return x; }
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(0);
 
-    int n ; 
-    int arr[n];
-    cinarr( arr , n );
-    for ( int i = 0;i < n; i++ )
-    {
-        arr[i] = nextint();
-    }
 
     return 0;
 }
