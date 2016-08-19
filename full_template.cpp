c601c302aa655f42364b7a7bb63d0fc95849a425
jaskamalkainth <kainthjaskamal@gmail.com>
2016-08-19 17:06:02 +0530

    update

--- full_template.cpp
+++ full_template.cpp
@@ -1,7 +1,5 @@
 /*
- *
  *    J1K7_7
- * 
  */
 #include <iostream>
 #include <sstream>
@@ -34,30 +32,30 @@ typedef pair<int,int>      pii;
 typedef pair<ll,ll>        pll;
 typedef vector<int>        vi;
 typedef vector<long long>  vll;
-#define l(x) 		       (x << 1) + 1
-#define r(x) 		       (x << 1) + 2
+#define left(x) 		   (x << 1)
+#define right(x) 		   (x << 1) + 1
 #define mid(l, r) 	       ((l + r) >> 1)
 #define mp                 make_pair
 #define pb                 push_back
 #define all(a)             a.begin(),a.end()
 #define debug(x)	       {cerr <<#x<<" = " <<x<<"\n"; }
 #define debug2(x, y)       {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<"\n";}
+#define debug3(x, y, z)    {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<", "<<#z<<" = "<<z<<"\n";}
+#define debug4(x, y, z, w) {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<", "<<#z<<" = "<<z<<", "<<#w << " = " <<w <<"\n";}
 #define ss                 second
 #define ff                 first
 #define m0(x) 		       memset(x,0,sizeof(x))
-#define snuke(c,itr)       for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
 
-template<class T>
-inline bool ispow2(T x){return (x!=0 && (x&(x-1))==0);} //0 or 1
+const int mod=1e9+7;
 
-template<class T> inline T powmod(T a,T b,T mod) {ll res = 1; while(b){if(b&1) res = (res*a)%mod;a = (a*a)%mod;b >>= 1;}return res;}
 template<class T> inline T gcd(T a,T b){ll t;while(b){a=a%b;t=a;a=b;b=t;}return a;}
 template<class T> inline T lcm(T a,T b){return a/gcd(a,b)*b;}
+template<typename T, typename S> T expo(T b, S e, const T &m){if(e <= 1)return e == 0?1: b;\
+	return (e&1) == 0?expo((b*b)%m, e>>1, m): (b*expo((b*b)%m, e>>1, m))%m;}
+template<typename T> T modinv(T a) { return expo(a, mod-2, mod); }
 
 inline int nextint(){ int x; scanf("%d",&x); return x; }
 inline ll nextll(){ ll x; scanf("%lld",&x); return x; }
-
-const int mod=1e9+7;
 
 const ll  mx_ll   = numeric_limits<ll> :: max();
 const int mx_int  = numeric_limits<int> :: max();
@@ -68,6 +66,10 @@ const long double PI = (long double)(3.1415926535897932384626433832795);
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(0);
+#ifndef __APPLE__
+    freopen("  .in", "rb", stdin);
+    freopen("  .out", "wb", stdout);
+#endif
 
 
     return 0;
