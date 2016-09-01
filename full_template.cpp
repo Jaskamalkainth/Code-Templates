e600d9b8ea094bec93adaf24992e4f5db518c702
jaskamalkainth <kainthjaskamal@gmail.com>
2016-09-02 03:39:56 +0530

    getchar tempalte added

--- full_template.cpp
+++ full_template.cpp
@@ -66,6 +66,12 @@ const int mx_int  = numeric_limits<int> :: max();
 
 const long double PI = (long double)(3.1415926535897932384626433832795);
 
+#define gc getchar
+template <typename T> void scanint(T &x) {
+	T c = gc(); while(((c < 48) || (c > 57)) && (c!='-')) c = gc();
+	bool neg = false; if(c == '-') neg = true; x = 0; for(;c < 48 || c > 57;c=gc());
+	for(;c > 47 && c < 58;c=gc())	x = (x*10) + (c - 48); if(neg)	x = -x;
+}
 
 int main()
 {
