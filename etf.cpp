1484b793f71a23de0672e697675eaa435e6659b9
jaskamalkainth <kainthjaskamal@gmail.com>
2016-09-13 05:01:29 +0530

    ETF update

--- etf.cpp
+++ etf.cpp
@@ -1,23 +1,20 @@
-
-const int maxN = 1e+7;
+const int maxN = 1e6+7;
 vi etf , P;
-void ETF()
+inline void ETF()
 {
     etf.resize(maxN+1);
     P.resize(maxN+1,0);
-    for(int i=1;i<=maxN;i++)
-        etf[i]=i;
-    for(int i=2;i<=maxN;i++)
+    for(int i = 1;i <= maxN; i++)
+        etf[i] = i;
+    for(int i = 2;i <= maxN; i++)
     {
         if(!P[i])
         {
-            etf[i]-=(etf[i]/i);
-            int j=2*i;
-            while( j <= maxN)
+            etf[i] -= (etf[i]/i);
+			for(int j = 2*i; j < maxN; j += i)
             {
-                etf[j]-=(etf[j]/i);
-                P[j]=1;
-                j+=i;
+                etf[j] -= (etf[j]/i);
+                P[j] = 1;
             }
         }
     }
