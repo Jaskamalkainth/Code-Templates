7b25b3a3d55619f68b9a33bf78951be13917b9c5
jaskamalkainth <kainthjaskamal@gmail.com>
2016-09-25 11:51:46 +0530

    added kth ancestor

--- lca.cpp
+++ lca.cpp
@@ -3,42 +3,49 @@
  */
 const int MAXN = 1e6+7; 
 const int MAXLOG = 20; 
+int par[MAXN][MAXLOG]; 
+vector<int> G[MAXN];
+int depth[MAXN];
 
-int par[MAXN][MAXLOG]; // initially all -1
-
-vi adj[MAXN];
-int d[MAXN];
-
-void dfs(int v,int p = -1){
+void dfs_lca(int v,int p = -1)
+{
     par[v][0] = p;
     if(p + 1)
-        d[v] = d[p] + 1;
+        depth[v] = depth[p] + 1;
     for(int i = 1;i < MAXLOG;i ++)
         if(par[v][i-1] + 1)
             par[v][i] = par[par[v][i-1]][i-1];
-    for(auto u : adj[v])
+    for(auto u : G[v])
         if(p - u)
-            dfs(u,v);
+            dfs_lca(u,v);
 }
-
-int lca(int v,int u){
-    if(d[v] < d[u])
+inline int lca(int v,int u)
+{
+    if(depth[v] < depth[u])
         swap(v,u);
-    for(int i = MAXLOG - 1;i >= 0;i --)
-        if(par[v][i] + 1 and d[par[v][i]] >= d[u])
+    for(int i = MAXLOG - 1;i >= 0; i--)
+        if(par[v][i] + 1 and depth[par[v][i]] >= depth[u])
             v = par[v][i];
     if(v == u)
         return v;
-    for(int i = MAXLOG - 1;i >= 0;i --)
+    for(int i = MAXLOG - 1;i >= 0; i--)
         if(par[v][i] - par[u][i])
             v = par[v][i], u = par[u][i];
     return par[v][0];
 }
-
-int dist(int u , int  v)
+inline int dist(int u , int  v)
 {
     int l = lca( u , v);
-    return ( d[u] + d[v] - 2 * d[l] );
+    return ( depth[u] + depth[v] - 2 * depth[l] );
+}
+inline int kth(int u , int k)
+{
+	if(k > depth[u])  
+		return -1;
+	for(int i = MAXLOG-1; i >= 0; i--)
+		if((1 << i) & k)
+			u = par[u][i];
+	return u;
 }
 
 
